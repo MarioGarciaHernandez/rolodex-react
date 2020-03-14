8e6a7edb056b68076415199abe9796cdeca7780f
Mario Garcia <mgarciah@psl.com.co>
2020-03-14 17:35:51 -0500

    Updates

---
+++ precache-manifest.d95a2609fcab83ceb0c29557aa87c1b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d3ec10435161a3c67b143d19c0bdc2a",
+    "url": "/rolodex-react/index.html"
+  },
+  {
+    "revision": "013280da4165ee1a8680",
+    "url": "/rolodex-react/static/css/main.02896e1b.chunk.css"
+  },
+  {
+    "revision": "ebea2ca50f5ac2a2ca9a",
+    "url": "/rolodex-react/static/js/2.ff87c9fa.chunk.js"
+  },
+  {
+    "revision": "013280da4165ee1a8680",
+    "url": "/rolodex-react/static/js/main.f4a0ec00.chunk.js"
+  },
+  {
+    "revision": "cb2c5193e7c328e3ce56",
+    "url": "/rolodex-react/static/js/runtime~main.5eabd68e.js"
+  }
+]);
